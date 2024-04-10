c6c12a0ecaeeb07f11f9e432c99a2089ccb651b7
ashley <ashleyjulianpillay@gmail.com>
2024-04-10 19:53:47 +0200

    add a second print statement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to rockPaperScissors");
+        System.out.println("Added a second print statement");
+
 
 
     }
